8ff963670879ebf2b13ad8077b9d495a26a885f0
ben <legoben1998@gmail.com>
2019-04-20 17:27:55 -0500

    premerge

--- chrome/background.js
+++ chrome/background.js
@@ -24,6 +24,10 @@ function base64ToBlob(base64, mime)
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
+    if(request.type !== "screenshot"){
+        return false;
+    }
+
     console.log("sent from tab.id=" + sender.tab.id);
 
     // sendResponse("sent from tab.id=" + sender.tab.id);
@@ -41,7 +45,72 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                     sendResponse(data);
                 })
         });
-    }, 1500);
+    }, 700);
 
     return true;
 });
+
+
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+    if(request.type !== "nlp"){
+        return false;
+    }
+
+    $.ajax({"url": "http://e44bd8fa.ngrok.io/api/text", method: "POST", data: {"text": request.text}})
+    .done(function (data) {
+        console.log("text dada", data);
+        sendResponse(data);
+    })
+});
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+    if(request.type !== "bias"){
+        return false;
+    }
+
+    $.ajax({"url": "http://e44bd8fa.ngrok.io/api/bias", method: "POST", data: {"text": request.name}})
+        .done(function (data) {
+            console.log("text dada", data);
+            sendResponse(data);
+        })
+});
+
+
+
+//
+//
+
+//
+//
+// const endpoint = "https://www.googleapis.com/youtube/v3/videos";
+// const key = "AIzaSyCaHWZYPuLv4cD6k-TQjg4Jx_1GQnG1wFw";
+//
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//
+//     if (request.type !== "tags") {
+//         return;
+//     }
+//
+//     fetchTags(request.videoId)
+//         .then(t => sendResponse({tags: t}))
+//         .catch(e => sendResponse({error: "Error fetching tags: " + e}));
+//
+//     return true; // tells the runtime not to close the message channel
+// });
+//
+//
+// const fetchTags = videoId => {
+//
+//     const url = `${endpoint}?part=snippet&fields=items(snippet(tags))&id=${encodeURIComponent(videoId)}&key=${key}`;
+//
+//     console.log("url", url);
+//
+//     return fetch(url)
+//         .then(console.log("resp", r))
+//         .then(r => (r.items[0] && r.items[0].snippet.tags) || []);
+// };
