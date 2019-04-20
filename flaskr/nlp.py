82eabe4815101cd22cac06baa41ba5249fd460a9
Estella <estellazx@gmail.com>
2019-04-20 14:48:17 -0500

    returns wikipeadia information

--- flaskr/nlp.py
+++ flaskr/nlp.py
@@ -2,9 +2,11 @@ import googleapiclient.discovery
 import argparse
 import json
 import sys
+import wikipedia
+from fuzzywuzzy import process as fuzzproc
+
 
 def analyze_entities(text, encoding='UTF32'):
-    text = "Joanne Rowling, who writes under the pen names J. K. Rowling and Robert Galbraith, is a British novelist and screenwriter who wrote the Harry Potter fantasy series."
     body = {
         'document': {
             'type': 'PLAIN_TEXT',
@@ -17,6 +19,35 @@ def analyze_entities(text, encoding='UTF32'):
 
     request = service.documents().analyzeEntities(body=body)
     response = request.execute()
-    print(json.dumps(response, indent=2)
-)
-    return json.dumps(response, indent=2)
+
+    entries = response['entities']
+
+    data = []
+
+    for word in entries:
+        if(word['type'] == 'PERSON' or word['type'] == 'ORGANIZATION'):
+            # only adds data if there is a wiki page for it
+            if('wikipedia_url' in word['metadata']):
+                data.append(_get_wiki_info(word['name'], word['metadata']['wikipedia_url'], True))
+    # print(data)
+
+    return json.dumps(data, indent=2)
+
+
+def _get_wiki_info(name, wiki, haspage):
+    info = {}
+    info['name'] = name
+    if not haspage:
+        page_name = wikipedia.search(name)[0]
+        print("u",page_name)
+    else:
+        page_name = wiki.split("/")[-1]
+    page = wikipedia.page(page_name)
+    summary = page.summary
+
+    info['url'] = wiki
+    info['summary'] = summary
+    info['short_summary'] = summary[:summary.index('.')+1]
+    info['image_url'] = fuzzproc.extractOne(name + ' Portrait', page.images)[0]
+    
+    return info
