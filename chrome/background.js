6cc35c095c2c952954079c1217c0554a87f4bc5a
ben <legoben1998@gmail.com>
2019-04-20 14:18:42 -0500

    chrome checkin

---
+++ chrome/background.js
@@ -0,0 +1,47 @@
+function base64ToBlob(base64, mime)
+{
+    mime = mime || '';
+    var sliceSize = 1024;
+    var byteChars = window.atob(base64);
+    var byteArrays = [];
+
+    for (var offset = 0, len = byteChars.length; offset < len; offset += sliceSize) {
+        var slice = byteChars.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    return new Blob(byteArrays, {type: mime});
+}
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+    console.log("sent from tab.id=" + sender.tab.id);
+
+    sendResponse("sent from tab.id=" + sender.tab.id);
+    setTimeout(function () {
+    chrome.tabs.captureVisibleTab(null, {format: 'png', quality: 100}, function (dataURI) {
+        // send to server
+
+        dataURI = dataURI.replace(/^data:image\/(png|jpg);base64,/, "");
+        var blob = base64ToBlob(dataURI, "img/png");
+        var formData = new FormData();
+        formData.append('img', blob);
+
+            $.ajax({"url": "http://e44bd8fa.ngrok.io/api/image", method: "POST", contentType: false, processData: false, data: formData})
+                .done(function (data) {
+                    console.log(data)
+                })
+        });
+    }, 1500)
+
+
+});
---
+++ chrome/yt.js
@@ -0,0 +1,45 @@
+let currentlyPaused = false;
+let delayPassed = false;
+
+setTimeout(function () {
+    delayPassed = true;
+},3000);
+
+
+function onPaused() {
+    currentlyPaused = true;
+    if(!delayPassed){
+        return
+    }
+
+    console.log("SIR");
+
+
+    $("#container > h1 > yt-formatted-string").text("paused");
+
+    chrome.runtime.sendMessage("screenshot", function(response) {
+        console.log(response);
+    });
+
+}
+
+
+function onPlayed() {
+    // alert("unpaused");
+    $("#container > h1 > yt-formatted-string").text("unpaused");
+    currentlyPaused = false;
+}
+
+console.log("hi");
+
+setInterval(function () {
+    // if pasued
+    if($("#movie_player").hasClass("paused-mode") && !currentlyPaused){
+        onPaused();
+
+    // was just unpaused
+    } else if (!$("#movie_player").hasClass("paused-mode") && currentlyPaused) {
+        onPlayed();
+    }
+
+}, 100);
